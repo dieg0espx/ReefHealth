142605a718ade670d68bbeb972a9f9c5da0ed78e
Diego Espinosa <espinosa9mx@gmail.com>
2025-07-16 18:22:29 +0200

    🎨 Configure complete favicon setup with all added favicon files
    - Update site.webmanifest to use web-app-manifest icons (192x192, 512x512, 96x96)
    - Add favicon.svg for modern browsers
    - Configure favicon-96x96.png for high-DPI displays
    - Set apple-touch-icon.png for iOS devices
    - Add theme-color meta tag for browser UI
    - Update manifest with proper branding and colors

--- pages/_document.js
+++ pages/_document.js
@@ -10,10 +10,11 @@ export default function Document() {
         
         {/* Favicon */}
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/site.webmanifest" />
+        <meta name="theme-color" content="#EE2966" />
         
         {/* Preconnect to external domains for performance */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
